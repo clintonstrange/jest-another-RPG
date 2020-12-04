a44483309288a807e6b735780e5c2e34be598ab5
clintonstrange <71712425+clintonstrange@users.noreply.github.com>
2020-12-04 11:07:16 -0600

    added getAttackValue, addPotion & usePotion methods

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -54,14 +54,41 @@ test("checks if player is alive or not", () => {
 });
 
 test("subtracts from player's health", () => {
+  const player = new Player("Dave");
+  const oldHealth = player.health;
+
+  player.reduceHealth(5);
+
+  expect(player.health).toBe(oldHealth - 5);
+
+  player.reduceHealth(99999);
+
+  expect(player.health).toBe(0);
+});
+
+test("gets player's attack value", () => {
+  const player = new Player("Dave");
+  player.strength = 10;
+
+  expect(player.getAttackValue()).toBeGreaterThanOrEqual(5);
+  expect(player.getAttackValue()).toBeLessThanOrEqual(15);
+});
+
+test('adds a potion to the inventory', () => {
     const player = new Player('Dave');
-    const oldHealth = player.health;
+    const oldCount = player.inventory.length;
   
-    player.reduceHealth(5);
+    player.addPotion(new Potion());
   
-    expect(player.health).toBe(oldHealth - 5);
+    expect(player.inventory.length).toBeGreaterThan(oldCount);
+  });
+
+  test('uses a potion from inventory', () => {
+    const player = new Player('Dave');
+    player.inventory = [new Potion(), new Potion(), new Potion()];
+    const oldCount = player.inventory.length;
   
-    player.reduceHealth(99999);
+    player.usePotion(1);
   
-    expect(player.health).toBe(0);
+    expect(player.inventory.length).toBeLessThan(oldCount);
   });
